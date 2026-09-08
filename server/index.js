ee6314c11afe998af74eda381eda899be82e48b8
gabrisandoval <luigi.sandoval99@gmail.com>
2026-09-08 22:47:13 +0200

    feat(core): Creazione host

--- server/index.js
+++ server/index.js
@@ -1,24 +1,40 @@
-const express = require('express')
-const http = require('http')
-const {Server} = require('socket.io')
-const path = require ('path')
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-io.on('connect', (socket) => {
-    console.log('Nuovo client connesso', socket.id);
+io.on('connection', (socket) => {
+    console.log('Nuovo client connesso:', socket.id);
+
+        // l'host chiede di creare una stanza
+        socket.on('host-create-room', () => {
+            const pin = generatePin();
+            console.log('Stanza creata con PIN:', pin);
+            socket.emit('room-created', { pin });
+        });
+
+        socket.on('disconnect', () => {
+            console.log('Client disconnesso:', socket.id);
+        });
+
+
+    // funzione per generare un PIN a 6 cifre
+    function generatePin() {
+        return Math.floor(100000 + Math.random() * 900000).toString();
+    }
 
     socket.on('disconnect', () => {
-        console.log('Client disconesso:', socket.id);
+        console.log('Client disconnesso:', socket.id);
     });
 });
 
 const PORT = 3000;
-app.listen(PORT, () =>{
+server.listen(PORT, () => {
     console.log(`Server avviato su http://localhost:${PORT}`);
 });
