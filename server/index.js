c003ae04fdb3c5bad73b0d70bc5d4672b45d85fe
gabrisandoval <luigi.sandoval99@gmail.com>
2026-09-08 18:27:11 +0200

    feat(core): Aggiungere Socket.IO

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const http = require('http')
+const {Server} = require('socket.io')
 const path = require ('path')
 
-const app = express()
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+io.on('connect', (socket) => {
+    console.log('Nuovo client connesso', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Client disconesso:', socket.id);
+    });
+});
 
 const PORT = 3000;
 app.listen(PORT, () =>{
